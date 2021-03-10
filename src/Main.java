7274164f506267bed1580c883bf9966bd9a66256
NadezhdaZykova <xilda777@gmail.com>
2021-03-10 19:48:06 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int transfer = 500_000_000;
+        int current_balance = 2_000_000_000;
+        int total = current_balance + transfer;
+        System.out.println("Total: " + total);
+    }
+}
